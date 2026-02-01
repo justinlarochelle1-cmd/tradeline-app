1c3cd9da65f618bb3411c24705115ecf28ab8e46
Justin LaRochelle <justin@Justins-MacBook-Air.local>
2026-02-01 16:16:06 -0600

    first version

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>Tradeline.ai</h1>
+      <p>Coming soon.</p>
+    </main>
+  );
+}
