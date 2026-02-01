9aa72d845aae33238a57cdd177b95fa7c8d97fa7
Justin LaRochelle <justin@Justins-MacBook-Air.local>
2026-02-01 17:46:06 -0600

    final homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     >
       <h1 style={{ fontSize: "48px", margin: 0 }}>Tradeline</h1>
       <p style={{ fontSize: "18px", marginTop: "12px", opacity: 0.7 }}>
-<h1>Tradeline coming soon</h1>
+<h1> coming soon</h1>
 <p>by Townes Parker</p>        
       </p>
     </main>
